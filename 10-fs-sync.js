3c5a606a33ee543713cd69e123eb67ef751ff643
sawzain <sawzain@gmail.com>
2026-05-14 03:38:10 +0545

    feat: fs-sync,fs-async, http

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,6 +1,6 @@
 //fs module
 const { readFileSync, writeFileSync } = require("fs");
-
+console.log("start");
 const first = readFileSync("./content/first.txt", "utf-8");
 const second = readFileSync("./content/second.txt", "utf-8");
 // console.log(first, second);
@@ -9,3 +9,5 @@ writeFileSync(
   `here is the result : ${first}, ${second}`,
   { flag: "a" },
 );
+console.log("done with the task");
+console.log("starting the next one");
